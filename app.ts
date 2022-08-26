ca020478a27c67def3409fb83e7a90d8b846c763
Owais-Sheikh <112202641+Owais-Sheikh@users.noreply.github.com>
2022-08-27 00:28:30 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,473 @@
+
+// ------------------------------- If-Else Exercise ------------------------------------//
+
+
+// ----------------------  Q # 1 -------------------------------//
+
+/*var num1:string | null = prompt("Enter first number: ");
+var num2:string | null = prompt("Enter second number: ");
+var num3:number = Number(num1);
+var num4:number = Number(char2);
+if (num3 > num4) {
+    console.log(num3," is maximum");
+}
+else{
+    console.log(num4 , "is maximum");
+}*/
+
+
+// ----------------------- Q # 2 -------------------------------//
+
+/*var num1:string | null = prompt("Enter first number: ");
+var num2:string | null = prompt("Enter second number: ");
+var num3:string | null = prompt("Enter third number: ")
+var num4:number = Number(num1);
+var num5:number = Number(num2);
+var num6:number = Number(num3)
+if (num4 > num5 && num4 > num6) {
+    console.log(num4,"is maximum");
+}
+else if(num5 > num4 && num5 > num6) {
+    console.log(num5,"is maximum");
+}
+else{
+    console.log(num6,"is maximum");
+    
+}*/
+
+// ----------------------------- Q # 3 -----------------------------//
+
+/*var num1:string | null = prompt("Enter any number: ")
+var num2:number = Number(num1);
+if (num2 > 0) {
+    console.log("Your number is Positive!!");
+}
+else if(num2 < 0) {
+    console.log("Your number is Negative!!");
+}
+else{
+    console.log("Your number is zero");
+}*/
+
+//---------------------------- Q # 4 -------------------------------//
+
+/*var num1:string | null = prompt("Enter any number: ")
+var num2:number = Number(num1);
+if(num2 % 5 == 0 && num2 % 11 == 0) {
+    console.log("Your number is divisible by 5 and 11");
+}
+else{
+    console.log("Your number is not divisible by 5 and 11");
+    
+}*/
+
+// -------------------------- Q # 5 ------------------------------//
+
+/*var num1:string | null = prompt("Enter any number: ");
+var num2:number = Number(num1);
+if (num2 % 2 == 0) {
+    console.log("Your number is even");
+    
+} else {
+    console.log("Your number is odd");
+    
+}*/
+
+// ------------------------- Q # 6 -----------------------------//
+
+/*var num1:string | null = prompt("Enter any year: ");
+var num2:number = Number(num1);
+if (num2 % 4 == 0) {
+    console.log("This is a leap year");
+    
+} else {
+    console.log("This ia not a leap year");
+    
+}*/
+
+// ------------------------ Q # 7 -------------------------------//
+
+/*var char1: string | null = prompt("Enter any alphabet: ")
+var char2: string = String(char1)
+if (char2 == 'A' || char2 == 'a' || char2 == 'B' || char2 == 'b' || char2 == 'C' || char2 == 'c' || char2 == 'D' || char2 == 'd' || char2 == 'E' || char2 == 'e' || char2 == 'F' || char2 == 'f' || char2 == 'G' || char2 == 'g' || char2 == 'H' || char2 == 'h' || char2 == 'I' || char2 == 'i' || char2 == 'J' || char2 == 'j' || char2 == 'K' || char2 == 'k' || char2 == 'L' || char2 == 'l' || char2 == 'M' || char2 == 'm' || char2 == 'N' || char2 == 'n' || char2 == 'O' || char2 == 'o' || char2 == 'P' || char2 == 'p' || char2 == 'Q' || char2 == 'q' || char2 == 'R' || char2 == 'r' || char2 == 'S' || char2 == 's' || char2 == 'T' || char2 == 't' || char2 == 'U' || char2 == 'u' || char2 == 'V' || char2 == 'v' || char2 == 'W' || char2 == 'w' || char2 == 'X' || char2 == 'x' || char2 == 'Y' || char2 == 'y' || char2 == 'Z' || char2 == 'z') {
+    console.log("Your character is alphabet");
+
+} else {
+    console.log("Your character is not alphabet");
+
+}*/
+
+// ---------------------- Q # 8 -------------------------------//
+
+/*var char1:string | null = prompt("Enter any alphabet: ")
+var char2:string = String(char1);
+if (char2 == "A" || char2 == "a" || char2 == "E" || char2 == "e" || char2 == "I" || char2 == "i" || char2 == "O" || char2 == "o" || char2 == "U" || char2 == "u" || char2 == "S" || char2 == "s" ) {
+    console.log("Your alphabet is vowel!!");
+    
+} else {
+    console.log("Your alphabet is consonunt");
+    
+}*/
+
+// --------------------- Q # 9 -------------------------------//
+
+/*var char1: string | null = prompt("Enter any alphabet: ")
+var char2: string = String(char1)
+if (char2 == 'A' || char2 == 'a' || char2 == 'B' || char2 == 'b' || char2 == 'C' || char2 == 'c' || char2 == 'D' || char2 == 'd' || char2 == 'E' || char2 == 'e' || char2 == 'F' || char2 == 'f' || char2 == 'G' || char2 == 'g' || char2 == 'H' || char2 == 'h' || char2 == 'I' || char2 == 'i' || char2 == 'J' || char2 == 'j' || char2 == 'K' || char2 == 'k' || char2 == 'L' || char2 == 'l' || char2 == 'M' || char2 == 'm' || char2 == 'N' || char2 == 'n' || char2 == 'O' || char2 == 'o' || char2 == 'P' || char2 == 'p' || char2 == 'Q' || char2 == 'q' || char2 == 'R' || char2 == 'r' || char2 == 'S' || char2 == 's' || char2 == 'T' || char2 == 't' || char2 == 'U' || char2 == 'u' || char2 == 'V' || char2 == 'v' || char2 == 'W' || char2 == 'w' || char2 == 'X' || char2 == 'x' || char2 == 'Y' || char2 == 'y' || char2 == 'Z' || char2 == 'z') {
+    console.log("Your character is alphabet");
+
+}
+    else if (char2 >= '0') {
+        console.log("Your character is a digit");
+        
+    }
+    
+
+else {
+    console.log("Your character is a special character");
+}*/
+
+// -------------------------- Q # 10 ---------------------------//
+
+/*var char1:string | null = prompt("Enter any alphabet: ");
+var char2:string = String(char1)
+if(char2 >= 'A' && char2 <= 'Z') {
+    console.log("Your entered character is uppercase alphabet!!!");
+    
+} else {
+    console.log("Your entered character is lowercase alphabet!!!");
+    
+}*/
+
+// -------------------------- Q # 11 ---------------------------//
+
+/*var week:string | null = prompt("Enter week number: ");
+var weeknum:number = Number(week);
+if (weeknum == 1) {
+    console.log("Monday");
+    
+}
+else if (weeknum == 2) {
+    console.log("Tuesday");
+    
+}
+else if (weeknum == 3) {
+    console.log("Wednesday");
+    
+}
+else if (weeknum == 4) {
+    console.log("Thursday");
+    
+}
+else if (weeknum == 5) {
+    console.log("Friday");
+    
+}
+else if (weeknum == 6) {
+    console.log("Saturday");
+    
+}
+else{
+    console.log("Sunday");
+    
+}*/
+
+
+// ------------------------- Q # 12 -------------------------- //
+
+
+/*var month:string | null = prompt("Enter month number: ");
+var monthnum:number = Number(month);
+if (monthnum <= 12) {
+    if (monthnum == 1) {
+        console.log("It contains 31 days");
+        
+    }
+    else if (monthnum == 2) {
+        console.log("It containes 28 days or 29 days");
+        
+    }
+    else if (monthnum == 3) {
+        console.log("It containes 31 days");
+        
+    }else if (monthnum == 4) {
+        console.log("It containes 30 days");
+        
+    }else if (monthnum == 5) {
+        console.log("It containes 31 days");
+        
+    }else if (monthnum == 6) {
+        console.log("It containes 30 days");
+        
+    }else if (monthnum == 7) {
+        console.log("It containes 31 days");
+        
+    }else if (monthnum == 8) {
+        console.log("It containes 31 days");
+        
+    }else if (monthnum == 9) {
+        console.log("It containes 30 days");
+        
+    }else if (monthnum == 10) {
+        console.log("It containes 31 days");
+        
+    }else if (monthnum == 11) {
+        console.log("It containes 30 days");
+        
+    }
+    else{
+        console.log("It containes 31 days");
+        
+    }
+}
+else{
+    console.log("Please enter a valid number!!!");
+    
+}*/
+
+// ------------------------- Q # 13 -------------------------- //
+
+/*var amount:string | null = prompt("Enter amount : ");
+var amountnum:number = Number(amount);
+var note5000 , note1000 , note500 , note100 , note50 , note20 , note10;
+note5000 = note1000 = note500 = note100 = note50 = note20 = note10 = 0
+if (amountnum >= 5000) {
+    note5000 = amountnum/5000;
+    console.log("Total number of 5000 note is ",note5000);
+}
+else if (amountnum >= 1000) {
+    note1000 = amountnum/1000;
+    console.log("Total number of 1000 note is ",note1000);
+
+}
+else if (amountnum >= 500) {
+    note500 = amountnum/500;
+    console.log("Total number of 500 note is ",note500);
+
+}
+else if (amountnum >= 100) {
+    note100 = amountnum/100;
+    console.log("Total number of 100 note is ",note100);
+
+}
+else if (amountnum >= 50) {
+    note50 = amountnum/50;
+    console.log("Total number of 50 note is ",note50);
+
+}
+else if (amountnum >= 20) {
+    note20 = amountnum/20;
+    console.log("Total number of 20 note is ",note20);
+
+}
+else{
+    note10 = amountnum/10;
+    console.log("Total number of 10 note is ",note10);
+
+}*/
+
+
+// ------------------------------ Q # 14 ----------------------- //
+
+/*var angle1:string | null = prompt("Enter first angle of trianle of triangle: ");
+var angletop:number = Number(angle1);
+var angle2:string | null = prompt("Enter second angle of trianle of triangle: ");
+var angleleft:number = Number(angle2);
+var angle3:string | null = prompt("Enter third angle of trianle of triangle: ");
+var angleright:number = Number(angle3);
+var totalangle = angletop + angleleft + angleright;
+if (totalangle == 180) {
+    console.log("Your triangle is valid!!!");
+    
+} else {
+    console.log("Your triangle is not valid!!!");
+    
+}*/
+
+
+// ----------------------------- Q # 15 -------------------------//
+
+/*var sideA:string | null = prompt("Enter first side of a triangle: ");
+var side1:number = Number(sideA);
+var sideB:string | null = prompt("Enter second side of a triangle: ");
+var side2:number = Number(sideB);
+var sideC:string | null = prompt("Enter third side of a triangle: ");
+var side3:number = Number(sideC);
+if (side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1) {
+    console.log("Triangle is valid!!");
+    
+}
+else{
+    console.log("Triangle is not valid!!!");
+    
+}*/
+
+
+// ---------------------------- Q # 16 ----------------------------//
+
+/*var angle1:string | null = prompt("Enter first angle of trianle of triangle: ");
+var angletop:number = Number(angle1);
+var angle2:string | null = prompt("Enter second angle of trianle of triangle: ");
+var angleleft:number = Number(angle2);
+var angle3:string | null = prompt("Enter third angle of trianle of triangle: ");
+var angleright:number = Number(angle3);
+var totalangle = angletop + angleleft + angleright;
+if (totalangle == 180) {
+    if ((angletop == 60) && (angleleft == 60) && (angleright == 60)) {
+        console.log("Triangle is Equilateral!!!");
+        
+    }
+    else if ((angletop == angleleft) || (angletop == angleright) || (angleleft == angleright)) {
+        console.log("Triangle is isosceles!!!");
+        
+    }
+    else{
+        console.log("Triangle is Scalene!!!");
+        
+    }
+}
+else{
+    console.log("Triangle is not valid!!!");
+    
+}*/
+
+
+// -------------------------------- Q # 17 ------------------------//
+
+
+/*var num1:string | null = prompt("Enter coffecient of a : ");
+var a:number = Number(num1);
+var num2:string | null = prompt("Enter coffecient of b : ");
+var b:number = Number(num2);
+var num3:string | null = prompt("Enter coffecient of c : ");
+var c:number = Number(num3);
+var dicriminant = b*b - 4*a*c;
+var x1,x2,realpart,imgpart;
+if (dicriminant > 0) {
+    console.log("Roots are real and different");
+    x1 = (-b + Math.sqrt(dicriminant)) / (2*a)
+    x2 = (-b - Math.sqrt(dicriminant)) / (2*a)
+    console.log("X1 = ",x1);
+    console.log("X2 = ",x2);
+    
+}
+else if (dicriminant == 0 ) {
+    console.log("Roots are real and equal");
+    x1 = -b / (2*a);
+    console.log("x1 = x2 = ",x1);
+    
+}
+else{
+    console.log("Roots are complex and different");
+    realpart = -b / (2*a);
+    imgpart = Math.sqrt(dicriminant) / (2*a);
+    console.log("x1 = ",realpart, "+" ,imgpart, "i");
+    console.log("x2 = ",realpart, "-" ,imgpart, "i");
+
+}*/
+
+// -------------------------------- Q # 18 -------------------------//
+
+/*var cost:string | null = prompt("Enter cost price: ");
+var costprice:number = Number(cost);
+var sell:string | null = prompt("Enter selling price: ");
+var sellingprice:number = Number(sell);
+var profit,loss;
+if (costprice < sellingprice) {
+    profit = sellingprice - costprice;
+    console.log("Profit is ",profit);
+    
+} else {
+    loss = costprice - sellingprice;
+    console.log("Loss is ",loss);
+    
+}*/
+
+// -------------------------------- Q # 19 -------------------------//
+
+
+/*var biology:string | null = prompt("Enter Biology marks: ");
+var bio:number = Number(biology);
+var Chemistry:string | null = prompt("Enter Chemistry marks: ");
+var chem:number = Number(Chemistry);
+var Physics:string | null = prompt("Enter Physics marks: ");
+var phy:number = Number(Physics);
+var Mathmatics:string | null = prompt("Enter Mathmatics marks: ");
+var math:number = Number(Mathmatics);
+var Computer:string | null = prompt("Enter Computer marks: ");
+var comp:number = Number(Computer);
+var percentage = (bio + chem + phy + math + comp) / 5.0;
+console.log("Percentage is ",percentage);
+
+if (percentage >= 90) {
+    console.log("A Grade!!!");
+    
+}
+else if (percentage >= 80) {
+    console.log("B Grade!!!");
+    
+}
+else if (percentage >= 70) {
+    console.log("C Grade!!!");
+    
+}
+else if (percentage >= 60) {
+    console.log("D Grade!!!");
+    
+}
+else if (percentage >= 40) {
+    console.log("E Grade!!!");
+    
+}
+else{
+    console.log("F Grade!!!");
+    
+}*/
+
+
+// ----------------------------------- Q # 20 --------------------------- //
+
+
+/*var basicsalary:string | null = prompt("Enter your basic salary: ");
+var salary:number = Number(basicsalary);
+var HRA ,DA,totalsalary;
+if (salary <= 10000) {
+    HRA = salary * 0.20;
+    DA = salary * 0.80;
+    
+}
+else if (salary <= 20000) {
+    HRA = salary * 0.25;
+    DA = salary * 0.90;
+    
+}
+else{
+    HRA = salary * 0.30;
+    DA = salary * 0.95;
+}
+    totalsalary = salary + HRA + DA;
+    console.log("Gross Salary is ",totalsalary);*/
+
+
+// ------------------------------- Q # 21 ----------------------- //
+
+
+/*var unitconsume:string | null = prompt("How many unit consumed by the customer: ");
+var unit:number = Number(unitconsume);
+var amount,totalbill,surcharge;
+if (unit <= 50) {
+    amount = unit * 0.5;
+} 
+else if (unit <= 150) {
+    amount = 25 + (unit - 50) * 0.75;
+}
+else if (unit <= 250) {
+    amount = 100 + (unit - 150) * 1.20;
+}
+else{
+    amount = 220 + (unit - 250) * 1.50;
+}
+surcharge = amount * 0.2;
+totalbill = amount + surcharge;
+console.log("Total bill is ",totalbill);*/
+
